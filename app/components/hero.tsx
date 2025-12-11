3cd4c95cfd39507205b13bb31fa0650815c04bc3
NabilApipp <nabilapipp@gmail.com>
2025-12-11 18:50:46 +0700

    update development page

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,8 +3,9 @@ export default function Hero() {
     return (
         <section id="hero">
             <div className="flex flex-col justify-center items-center mt-9">
-                <img src="/image/dev-icon.png" alt="Development Icon" className="w-[300px]"/>
-                <h1 className="mt-[20px] font-extrabold text-5xl">Halaman Masih Dalam Masa Development</h1>
+                <img src="image\construction.png" alt="Development Icon" className="w-[300px]"/>
+                <h1 className="mt-[20px] font-extrabold text-5xl">Halaman Masih Dalam Tahap Pengembangan</h1>
+                <h1 className="mt-[5px] font-extrabold text-3xl">🙏🏻🙏🏻🙏🏻</h1>
                 <p className="mt-[200px]"><span>&copy; {year}</span> XIIRPL 3'23 Team</p>
             </div>
         </section>
