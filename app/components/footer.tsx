f999fd583535691b49d2ab6c6b812680b1b707e4
NabilApipp <nabilapipp@gmail.com>
2025-12-11 18:28:16 +0700

    add devlopment page in hero

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="flex justify-center items-center"> 
-            <h2>Test Pesan</h2>
+            {/* <h2>Test Pesan</h2> */}
         </div>
         )
     }
--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,7 +1,12 @@
 export default function Hero() {
+     const year = new Date().getFullYear();
     return (
         <section id="hero">
-            <h1>Hero Section</h1>
+            <div className="flex flex-col justify-center items-center mt-9">
+                <img src="/image/dev-icon.png" alt="Development Icon" className="w-[300px]"/>
+                <h1 className="mt-[20px] font-extrabold text-5xl">Halaman Masih Dalam Masa Development</h1>
+                <p className="mt-[200px]"><span>&copy; {year}</span> XIIRPL 3'23 Team</p>
+            </div>
         </section>
     )
 }
