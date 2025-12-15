ed88eadbaa9947bb61999e8544ae6c7833a4cb9b
Alantaaa <lintangadyaalanta070@gmail.com>
2025-12-15 13:10:14 +0700

    fix navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
   ];
   return (
     <>
-      <nav className="bg-[#395886] text-white">
+      <nav className="fixed top-0 left-0 w-full bg-[#395886] text-white z-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-3">
