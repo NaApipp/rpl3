3378aa386d8fed51fda8ff9f8a6d20f72664a022
NabilApipp <nabilapipp@gmail.com>
2025-12-17 15:12:56 +0700

    fixing style hero page

--- app/components/backgroundSlide.tsx
+++ app/components/backgroundSlide.tsx
@@ -8,16 +8,22 @@ const images = [
   "asset/image/hero/bg-hero-3.png",
 ];
 
-export default function backgroundSlide() {
+export default function BackgroundSlider() {
+  const [mounted, setMounted] = useState(false);
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
+    setMounted(true);
+
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % images.length);
-    }, 5000); // ganti tiap 5 detik
+    }, 5000);
 
     return () => clearInterval(interval);
   }, []);
+
+  // ⛔ Jangan render apa pun sebelum client siap
+  if (!mounted) return null;
 
   return (
     <div className="absolute inset-0 -z-10 overflow-hidden">
@@ -27,9 +33,7 @@ export default function backgroundSlide() {
           className={`absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ${
             index === current ? "opacity-100" : "opacity-0"
           }`}
-          style={{
-            backgroundImage: `url(${img})`,
-          }}
+          style={{ backgroundImage: `url(${img})` }}
         />
       ))}
     </div>
--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,11 +1,19 @@
-import backgroundSlide from "./backgroundSlide";
+import BackgroundSlider from "./backgroundSlide";
+// import StrukturKelasPage from "./struktur-kelas/strukturKelas";
+import Link from "next/link";
 
 export default function Hero() {
     return (
-        <section id="hero" className="relative min-h-screen flex items-center justify-center text-white">
-            <backgroundSlide />
+        <section id="home" className="relative min-h-screen flex items-center justify-center text-white">
+            {/* Background Slide Function */}
+            <BackgroundSlider />
 
-            <div className=""></div>
-        </section>
+            <div className="z-10 text-center flex gap-30 flex-col justify-center items-center">
+                <h1 className="text-white font-poppins font-extrabold text-[70px] underline underline-offset-8">XII RPL 3'23</h1>
+                <p className="text-white font-poppins font-semibold text-[20px]">Kelas Rekayasa Perangkat Lunak <br /> yang inovatif dan kreatif, dan suka ajak ribut</p>
+                {/* Button direct section struktur kelas */}
+                <Link href="#struktur-Kelas" className="p-2 border border-4 w-[50%] font-poppins font-semibold text-[20px] rounded-[20px]">Lihat Lainnya</Link>
+            </div>
+        </section> 
     )
 }
