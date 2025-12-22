7a130545a4f6992eaec5d753a2dc8ae635192da3
NabilApipp <nabilapipp@gmail.com>
2025-12-22 16:03:21 +0700

    implement count-up library

--- app/components/count.tsx
+++ app/components/count.tsx
@@ -1,18 +1,27 @@
+"use client";
+import CountUp from "react-countup";
+
 export default function Count() {
-    return (
-        <div className="container-count flex bg-[#638ECB]  rounded-lg text-white">
-            <div className="aktif text-center">
-                <p className="count">36</p>
-                <p className="head-count">SISWA AKTIF</p>
-            </div>
-            <div className="laki text-center">
-                <p className="count">15</p>
-                <p className="head-count">LAKI-LAKI</p>
-            </div>
-            <div className="perempuan text-center">
-                <p className="count">21</p>
-                <p className="head-count">PEREMPUAN</p>
-            </div>
-        </div>
-    )
-} 
+  return (
+    <div className="container-count flex bg-[#638ECB]  rounded-lg text-white">
+      <div className="aktif text-center">
+        <p className="count">
+          <CountUp end={36} duration={7} scrollSpyOnce />
+        </p>
+        <p className="head-count">SISWA AKTIF</p>
+      </div>
+      <div className="laki text-center">
+        <p className="count">
+          <CountUp end={15} duration={7} scrollSpyOnce />
+        </p>
+        <p className="head-count">LAKI-LAKI</p>
+      </div>
+      <div className="perempuan text-center">
+        <p className="count">
+          <CountUp end={21} duration={7} scrollSpyOnce />
+        </p>
+        <p className="head-count">PEREMPUAN</p>
+      </div>
+    </div>
+  );
+}
